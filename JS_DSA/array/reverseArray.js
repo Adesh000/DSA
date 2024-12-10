894364e37fcbae29fa35bffcb165ad6394cd707c
Adesh Nayak <adesh@krenai.com>
2024-12-10 21:52:12 +0530

    reversed using swap method

--- JS_DSA/array/reverseArray.js
+++ JS_DSA/array/reverseArray.js
@@ -9,3 +9,15 @@ const reverseArray = (arr) => {
 };
 
 console.log(reverseArray([4, 6, 7, 9, 3, 5]));
+
+const reverseArr = (arr) => {
+  for (let i = 0; i < Math.floor((arr.length - 1) / 2); i++) {
+    let temp = arr[i];
+    console.log("first", temp);
+    arr[i] = arr[arr.length - (1 + i)];
+    arr[arr.length - (1 + i)] = temp;
+  }
+  console.log("Swapped items:", arr);
+};
+
+reverseArr([1, 3, 5, 8, 1, 0]);
