d285b616573f021b2d3d391ea01b233f70d235c5
Adesh Nayak <adesh@krenai.com>
2024-12-16 23:38:57 +0530

    kadens approach

--- JS_DSA/array/kadens.js
+++ JS_DSA/array/kadens.js
@@ -15,3 +15,17 @@ const largestSubArraySum = (arr) => {
 largestSubArraySum([2, -7, 2, 4, -2, 1, 0]);
 
 // O(n^2) TC
+
+const kadensApproach = (arr) => {
+  let maxSum = arr[0];
+  let maxEnding = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+    maxSum = Math.max(maxSum, maxEnding);
+  }
+
+  console.log("Max sum", maxSum);
+};
+
+kadensApproach([2, -5, 6, 7, -3, -8, 9, 5]);
