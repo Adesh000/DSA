04442c897725fc36f57b72fbff985e67c0b61f2f
Adesh Nayak <adesh@krenai.com>
2024-12-23 23:43:37 +0530

    duplicate element and methods to find the key in obj

--- JS_DSA/array/repeatedNumber.js
+++ JS_DSA/array/repeatedNumber.js
@@ -11,3 +11,42 @@ const repeatedNumber = (arr) => {
 };
 
 console.log("Repeated Number", repeatedNumber([1, 4, 1, 7, 9, 6]));
+
+const duplicateNumber = (arr) => {
+  let obj = {};
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (obj.hasOwnProperty(arr[i])) {
+      console.log("Duplicate element: ", arr[i]);
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+};
+
+duplicateNumber([1, 3, 4, 2, 2]);
+
+// To check wether the key is present in the obj or not
+
+// 1.
+let obj = {
+  id: 1,
+  name: "adesh",
+};
+
+const log1 = "id" in obj;
+const log2 = "place" in obj;
+
+console.log("Log one", log1);
+console.log("Log two", log2);
+
+// 2.
+const output1 = obj.hasOwnProperty("name");
+
+console.log("Output", output1);
+
+//3.
+const output2 = Object.keys(obj).includes("address");
+const output3 = Object.keys(obj).includes("name");
+
+console.log("Output 2: ", output2);
+console.log("Output 3: ", output3);
